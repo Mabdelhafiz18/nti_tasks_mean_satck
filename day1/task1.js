71cb845cc3b5c81cd0866aa965d14e3565dcccb4
Mabdelhafiz18 <you@example.com>
2025-08-04 14:37:34 +0300

    task day 1 done

---
+++ day1/task1.js
@@ -0,0 +1,31 @@
+//  Q1
+
+var x = 10;
+var y = "MO";
+var z = true;
+var a= null;
+var b = undefined;
+
+// Q2
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
+
+// q3
+
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+console.log(typeof(a));
+console.log(typeof(b));
+
+// Q4
+var fname = "mohamed";
+var lname = "abdelhafiz";
+
+console.log(fname +" " + lname);
+
+console.log(`${fname} ${lname}`);
